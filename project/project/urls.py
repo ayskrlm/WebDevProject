e88c9ce873c005e3f4b0f0984b085be4e729cb1f
apollotesting <antiponaclarence@gmail.com>
2024-12-06 20:31:38 +0800

    uop

--- project/project/urls.py
+++ project/project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('apollos', include('apollos.urls')),
+    path('', include('apollos.urls')),
 ]

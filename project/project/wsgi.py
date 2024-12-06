1c14a4cd767b883886003fef1e571cc7d7626c5e
apollotesting <antiponaclarence@gmail.com>
2024-12-06 20:53:04 +0800

    test

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -7,7 +7,7 @@ sys.path.append('/opt/render/project/src/project')  # Adjust if needed
 # Debugging line to print sys.path
 print(sys.path)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.project.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
